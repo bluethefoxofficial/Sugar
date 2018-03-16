b0b707c31d93e22fe776111a39b2e0f93be900ed
bluethefox <kessanro@hotmail.com>
2018-03-16 20:05:20 +0000

    Update browse.php

--- src/browse.php
+++ src/browse.php
@@ -11,66 +11,7 @@
 	</head>
 		<body>
 		<center><h3 class="display-4">Browse</h3></center>
- <?php
-         $dbhost = 'localhost:3036';
-         $dbuser = 'root';
-         $dbpass = 'rootpassword';
-         
-         $rec_limit = 10;
-         $conn = mysql_connect($dbhost, $dbuser, $dbpass);
-         
-         if(! $conn ) {
-            die('Could not connect: ' . mysql_error());
-         }
-         mysql_select_db('test_db');
-         
-         /* Get total number of records */
-         $sql = "SELECT count(emp_id) FROM employee ";
-         $retval = mysql_query( $sql, $conn );
-         
-         if(! $retval ) {
-            die('Could not get data: ' . mysql_error());
-         }
-         $row = mysql_fetch_array($retval, MYSQL_NUM );
-         $rec_count = $row[0];
-         
-         if( isset($_GET{'page'} ) ) {
-            $page = $_GET{'page'} + 1;
-            $offset = $rec_limit * $page ;
-         }else {
-            $page = 0;
-            $offset = 0;
-         }
-         
-         $left_rec = $rec_count - ($page * $rec_limit);
-         $sql = "SELECT emp_id, emp_name, emp_salary ". 
-            "FROM employee ".
-            "LIMIT $offset, $rec_limit";
-            
-         $retval = mysql_query( $sql, $conn );
-         
-         if(! $retval ) {
-            die('Could not get data: ' . mysql_error());
-         }
-         
-         while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
-            echo "EMP ID :{$row['emp_id']}  <br> ".
-               "EMP NAME : {$row['emp_name']} <br> ";
-         }
-         
-         if( $page > 0 ) {
-            $last = $page - 2;
-            echo "<a href = \"$_PHP_SELF?page = $last\">Last 10 Records</a> |";
-            echo "<a href = \"$_PHP_SELF?page = $page\">Next 10 Records</a>";
-         }else if( $page == 0 ) {
-            echo "<a href = \"$_PHP_SELF?page = $page\">Next 10 Records</a>";
-         }else if( $left_rec < $rec_limit ) {
-            $last = $page - 2;
-            echo "<a href = \"$_PHP_SELF?page = $last\">Last 10 Records</a>";
-         }
-         
-         mysql_close($conn);
-      ?>
+
 		</body>
 		<footer>
 		<center>&copy; <?php print($copyright); ?></center>
